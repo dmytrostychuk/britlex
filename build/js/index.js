0eddbbe24217c574703e24ebfb3fba9076361b3b
Dmitriy Stychuk <fenderchord@gmail.com>
2022-05-22 20:25:32 +0300

    add menu-burger content

--- build/js/index.js
+++ build/js/index.js
@@ -1,10 +1,10 @@
+let burgerBth = document.querySelector('.burger-bth');
+let navList = document.querySelector('.menu__nav');
 
-let burgerBth = document.querySelector(".burger-bth");
-let navList = document.querySelector(".menu__nav");
-
-if(burgerBth) {
-	burgerBth.addEventListener("click", function (e) {
-		navList.classList.toggle("menu__nav-active");
-		burgerBth.classList.toggle("burger-bth-active");
-	})
+if (burgerBth) {
+  burgerBth.addEventListener('click', function (e) {
+    document.body.classList.toggle('lock');
+    navList.classList.toggle('menu__nav-active');
+    burgerBth.classList.toggle('burger-bth-active');
+  });
 }
