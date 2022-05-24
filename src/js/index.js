9d5988208ea6ae3c75065bdd01c1d2e47611549a
Dmitriy Stychuk <fenderchord@gmail.com>
2022-05-24 22:47:32 +0300

    finished adaptive menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 let burgerBth = document.querySelector('.burger-bth');
-let navList = document.querySelector('.menu__nav');
+let menuNav = document.querySelector('.menu__nav');
+let menuBtn = document.querySelector('.menu__btn');
 
 if (burgerBth) {
   burgerBth.addEventListener('click', function (e) {
     document.body.classList.toggle('lock');
-    navList.classList.toggle('menu__nav-active');
+    menuNav.classList.toggle('menu__nav-active');
     burgerBth.classList.toggle('burger-bth-active');
+    menuBtn.classList.toggle('menu__btn-active');
   });
 }
