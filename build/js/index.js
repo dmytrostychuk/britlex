1aaf3c7dddec0ab201317e1dd8ca021b7bee81d5
Dmitriy Stychuk <fenderchord@gmail.com>
2022-07-03 19:44:37 +0300

    add smooth scrolling with js

--- build/js/index.js
+++ build/js/index.js
@@ -10,3 +10,25 @@ if (burgerBth) {
     menuBtn.classList.toggle('menu__btn-active');
   });
 }
+
+const menuLink = document.querySelectorAll('.menu__link');
+
+for (let menuLinks of menuLink) {
+  menuLinks.addEventListener('click', function (e) {
+    if (menuNav) {
+      document.body.classList.toggle('lock');
+      menuNav.classList.remove('menu__nav-active');
+      burgerBth.classList.remove('burger-bth-active');
+      menuBtn.classList.remove('menu__btn-active');
+    }
+
+    e.preventDefault();
+
+    const blockID = menuLinks.getAttribute('href');
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
